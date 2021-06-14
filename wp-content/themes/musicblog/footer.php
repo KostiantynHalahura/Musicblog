2391c6315a732d6a8cc0f1a1d44537d868be6ade
Kostiantyn Halahra <kostya.galagura@gmail.com>
2021-06-14 12:05:30 +0300

    Footer block added
    + footer template
    + footer styles
    + function for footer include

---
+++ wp-content/themes/musicblog/footer.php
@@ -0,0 +1,34 @@
+</div><!-- #content -->
+<footer>
+	<div>
+		<nav class="archives">
+			<h3>Архивы</h3>
+			<ul>
+				<li><a href="">Июнь 2021</a></li>
+				<li><a href="">Май 2021</a></li>
+				<li><a href="">Апрель 2021</a></li>
+			</ul>
+		</nav>
+	
+		<nav class="heading">
+			<h3>Рубрики</h3>
+			<ul>
+				<li><a href="/">Рубрика 1</a></li>
+				<li><a href="">Рубрика 2</a></li>
+			</ul>
+		</nav>
+	
+		<nav class="meta">
+			<h3>Мета</h3>
+			<ul>
+				<li><a href="">О нас</a></li>
+				<li><a href="">Комментарии</a></li>
+				<li><a href="">Статьи</a></li>
+			</ul>
+		</nav>
+	</div>
+</footer>
+<?php wp_footer(); ?>
+
+</body>
+</html>
