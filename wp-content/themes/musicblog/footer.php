1eec07eb7c6d49829704dad22e9593657e10e8a6
Kostiantyn Halahra <kostya.galagura@gmail.com>
2021-06-16 01:59:42 +0300

    Footer refactored
    + footer register function
    * footer display

--- wp-content/themes/musicblog/footer.php
+++ wp-content/themes/musicblog/footer.php
@@ -1,32 +1,6 @@
 </div><!-- #content -->
-<footer>
-	<div>
-		<nav class="archives">
-			<h3>Архивы</h3>
-			<ul>
-				<li><a href="">Июнь 2021</a></li>
-				<li><a href="">Май 2021</a></li>
-				<li><a href="">Апрель 2021</a></li>
-			</ul>
-		</nav>
-	
-		<nav class="heading">
-			<h3>Рубрики</h3>
-			<ul>
-				<li><a href="/">Рубрика 1</a></li>
-				<li><a href="">Рубрика 2</a></li>
-			</ul>
-		</nav>
-	
-		<nav class="meta">
-			<h3>Мета</h3>
-			<ul>
-				<li><a href="">О нас</a></li>
-				<li><a href="">Комментарии</a></li>
-				<li><a href="">Статьи</a></li>
-			</ul>
-		</nav>
-	</div>
+<footer id="footer" class="footer">
+	<?php wp_nav_menu(array( 'theme_location' => 'footer-menu', 'menu_class' => 'link-list last-child' ));?>
 </footer>
 <?php wp_footer(); ?>
 
