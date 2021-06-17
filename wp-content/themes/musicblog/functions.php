6325bc02570e14d40f98e64d25c1a12bcefbfddf
Kostiantyn Halahra <kostya.galagura@gmail.com>
2021-06-18 00:15:10 +0300

    Custom types was added
    +Band type
    + Quote type
    * Basic template was changed

--- wp-content/themes/musicblog/functions.php
+++ wp-content/themes/musicblog/functions.php
@@ -8,4 +8,38 @@
 		);
 	}
 	add_action( 'init', 'register_my_menus' );
+
+	function k_create_post_type_band() {
+		register_post_type( 'band',
+		array(
+			'labels' => array(
+				'name' => __( 'Bands' ),
+				'singular_name' => __( 'Band' ),
+			),
+			'public' => true,
+			'has_archive' => false,
+			'rewrite' => array('slug' => 'bands'),
+			'menu_position' => 4,
+			'supports' => array( 'title', 'editor', 'thumbnail', 'post_formats' ),
+		)
+	);
+}
+	add_action( 'init', 'k_create_post_type_band' );
+
+	function k_create_post_type_quote() {
+		register_post_type( 'quote',
+		array(
+			'labels' => array(
+				'name' => __( 'Quotes' ),
+				'singular_name' => __( 'Quote' ),
+			),
+			'public' => true,
+			'has_archive' => false,
+			'rewrite' => array('slug' => 'quotes'),
+			'menu_position' => 4,
+			'supports' => array( 'title', 'editor', 'post_formats' ),
+		)
+	);
+}
+	add_action( 'init', 'k_create_post_type_quote' );
 ?>
