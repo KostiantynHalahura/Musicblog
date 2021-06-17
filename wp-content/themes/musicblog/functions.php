f30a18e8f207ddb073861c55db53542b9b11af42
Kostiantyn Halahra <kostya.galagura@gmail.com>
2021-06-17 17:26:03 +0300

    Site perfomance increased
    + hooks for update swithching of

--- wp-content/themes/musicblog/functions.php
+++ wp-content/themes/musicblog/functions.php
@@ -1,4 +1,10 @@
 <?php
+	add_filter( 'auto_update_core', '__return_false' );
+	add_filter( 'auto_update_theme', '__return_false' );
+	add_filter( 'auto_update_plugin', '__return_false' );
+	add_filter( 'auto_update_translation', '__return_false' );
+	add_filter( 'auto_core_update_send_email', '__return_false' );
+
 	function register_my_menus() {
 		register_nav_menus(
 			array(
