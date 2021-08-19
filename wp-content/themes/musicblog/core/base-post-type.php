a1e48bb55c068067ca900c1ec7efd347c6608e25
Kostiantyn Halahra <kostya.galagura@gmail.com>
2021-08-19 12:01:55 +0300

    Post types fix
    * Approach for post types realization changed

--- wp-content/themes/musicblog/core/base-post-type.php
+++ wp-content/themes/musicblog/core/base-post-type.php
@@ -3,11 +3,13 @@ class BasePostType {
 	protected $name;
 	protected $args;
 
-	private function construct() {
+	public function construct() {
+		
 		add_action( 'init', [$this, 'register'] );
 	}
 
 	public function register() {
+		
 		register_post_type( $this->$name, $this->$args);
 	}
 }
