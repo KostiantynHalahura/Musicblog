52dc7c96dfc046f033166638c4cc59749bae77e2
Kostiantyn Halahra <kostya.galagura@gmail.com>
2021-08-30 00:53:36 +0300

    Launches archive added
    + Launch post view

---
+++ wp-content/themes/musicblog/archive-launch.php
@@ -0,0 +1,28 @@
+<?php get_header(); ?>
+	<section class="launches">
+		<?php
+		if ( have_posts() ) :
+			while ( have_posts() ) :
+				the_post();
+				?>
+				<article>
+                        <p>Название:</p>
+					    <h2><a href="<?php the_permalink() ?>"><?php the_title(); ?></a></h2>
+                        <p>Предстоящий:</p>
+                        <span class="upcoming"><?php echo get_post_meta( get_the_id(), 'upcoming', TRUE ); ?></span>
+                        <p>Ракета:</p>
+                        <span class="rocket"><?php echo get_post_meta( get_the_id(), 'rocket', TRUE ); ?></span>
+                        <p>Дата отправки:</p>
+                        <span class="date_utc"><?php echo get_post_meta( get_the_id(), 'date_utc', TRUE ); ?></span>
+                        <p>Предстоящий:</p>
+                        <span class="upcoming"><?php echo get_post_meta( get_the_id(), 'upcoming', TRUE ); ?></span>
+                        <p>Номер полёта:</p>
+                        <span class="flight_number"><?php echo get_post_meta( get_the_id(), 'flight_number', TRUE ); ?></span>
+                        <hr>
+				</article>
+			<?php
+			endwhile;
+		endif;
+		?>
+	</section>
+<?php get_footer(); ?>
